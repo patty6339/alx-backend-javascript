587ed620a9134d6e1ae2335c2c57906d6b665990
Patrick <patty6339@gmail.com>
2024-10-02 19:04:55 +0300

    corrected

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -8,3 +8,4 @@ export default function appendToEachArrayValue(array, appendString) {
   
     return myarray;
   }
+  
--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -6,3 +6,4 @@ export default function createIteratorObject(report) {
   
     return myarray;
   }
+  
