d567835a00dd0e7f76ffcd8bcab36d0807fba145
Patrick <patty6339@gmail.com>
2024-11-30 06:34:50 +0300

    node js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Prints the given message to STDOUT.
+ *
+ * @param {msg} str - The message to be printed.
+ */
+function displayMessage(msg) {
+    console.log(msg);
+  }
+  
+  module.exports = displayMessage;
+  
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,1 @@
+This is for the 0x05-Node_JS_basic project 
